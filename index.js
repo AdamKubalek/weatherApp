f3edffd4b6755c933caa24c75d42c1255397361e
AdamKubalek <stsaffp@email.cz>
2023-04-04 21:00:31 +0200

    Added JS search

---
+++ index.js
@@ -0,0 +1,36 @@
+const container = document.querySelector(".container");
+const search = document.querySelector(".search");
+const weatherBox = document.querySelector(".weather-box");
+const weatherDetails = document.querySelector(".weather-details");
+const error404 = document.querySelector(".not-found");
+
+search.addEventListener("click", () => {
+  const APIKey = "68a0f1d0a679cb6b2a5c068b0ea07bc8";
+  const city = document.querySelector(".search-box input").value;
+
+  if (city === "") return;
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.cod === "404") {
+        container.style.height = "400px";
+        weatherBox.style.display = "none";
+        weatherDetails.style.display = "none";
+        error404.style.display = "block";
+        error404.classList.add("fadeIn");
+        return;
+      }
+
+      error404.style.display = "none";
+      error404.classList.remove("fadeIn");
+      
+      const image = document.querySelector(".weather-box img");
+      const temprature = document.querySelector(".weather-box .temprature");
+      const description = document.querySelector(".weather-box .description");
+
+    });
+
+});

0af45900433af81a96e68b6556899116f8e7c6a1
AdamKubalek <stsaffp@email.cz>
2023-04-05 22:13:51 +0200

    Basic  index.js doen

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const container = document.querySelector(".container");
-const search = document.querySelector(".search");
+const search = document.querySelector(".search-box button");
 const weatherBox = document.querySelector(".weather-box");
 const weatherDetails = document.querySelector(".weather-details");
 const error404 = document.querySelector(".not-found");
@@ -7,6 +7,7 @@ const error404 = document.querySelector(".not-found");
 search.addEventListener("click", () => {
   const APIKey = "68a0f1d0a679cb6b2a5c068b0ea07bc8";
   const city = document.querySelector(".search-box input").value;
+  console.log(city)
 
   if (city === "") return;
 
@@ -26,11 +27,45 @@ search.addEventListener("click", () => {
 
       error404.style.display = "none";
       error404.classList.remove("fadeIn");
-      
+
       const image = document.querySelector(".weather-box img");
-      const temprature = document.querySelector(".weather-box .temprature");
+      const temperature = document.querySelector('.weather-box .temperature');
       const description = document.querySelector(".weather-box .description");
+      const humidity = document.querySelector(
+        ".weather-details .humidity span"
+      );
+      const wind = document.querySelector(".weather-details .wind span");
+
+      switch (data.weather[0].main) {
+        case "Clouds":
+          image.src = "images/cloudy.png";
+          break;
+        case "Clear":
+          image.src = "images/clear.png";
+          break;
+        case "Rain":
+          image.src = "images/rainy.png";
+          break;
+        case "Snow":
+          image.src = "images/snowy.png";
+          break;
+        case "haze":
+          image.src = "images/haze.png";
+          break;
+        default:
+          image.src = ""; 
+      }
+
+      temperature.innerHTML = `${parseInt(data.main.temp - 273)}°C`; 
+      description.innerHTML = `${data.weather[0].description}`;
+      humidity.innerHTML = `${data.main.humidity}%`;
+      wind.innerHTML = `${parseInt(data.wind.speed)} Km/h`;
+
+      weatherBox.style.display = "";
+      weatherDetails.style.display = "";
+      weatherBox.classList.add("fadeIn");
+      weatherDetails.classList.add("fadeIn");
+      container.style.height = "590px";
 
     });
-
 });
